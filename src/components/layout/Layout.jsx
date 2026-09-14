c504ac526b653fed748f5f84cfd9b4c4d7c938e9
zuriel337 <128194013+zuriel337@users.noreply.github.com>
2026-09-14 19:28:11 +0300

    Mount 2029 transition notice on home routes

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LayoutCore from "./LayoutCore.jsx";
 import FeatureSurfaceSync from "../FeatureSurfaceSync.jsx";
+import HomeTransitionNotice from "../HomeTransitionNotice.jsx";
 
 // One global projection bridge: public references consume canonical feature availability.
 export default function Layout(props) {
   return (
     <>
       <FeatureSurfaceSync />
+      <HomeTransitionNotice />
       <LayoutCore {...props} />
     </>
   );
